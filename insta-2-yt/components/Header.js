e92b2b56e7eb1de17af3319f472266b68cf04cd2
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-10-14 15:43:53 -0700

    finished with the right side of the header component

--- insta-2-yt/components/Header.js
+++ insta-2-yt/components/Header.js
@@ -2,16 +2,17 @@ import Image from "next/image";
 import {
   SearchIcon,
   PlusCircleIcon,
-  UseGroupIcon,
+  UserGroupIcon,
   HeartIcon,
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div>
-      <div className="flex justify-between max-w-6xl">
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50" >
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
@@ -30,21 +31,41 @@ function Header() {
         </div>
 
         {/* middle - search input field */}
-        <div className="relative mt-1 p-3 rounder-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
-          </div>
-          <input
-            className="bg-gray-50 block w-full pl-10 sm:text-sm 
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounder-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm 
             border-gray-300 focus:ring-black focus:border-black rounded-md"
-            type="text"
-            placeholder="Search"
-          />
+              type="text"
+              placeholder="Search"
+            />
+          </div>
         </div>
 
         {/* right  */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full 
+            flex items-center justify-center animate-pulse text-white">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
 
-        
+          <img
+            src="https://cdn1.mecum.com/auctions/sc0516/sc0516-243304/images/sc0516-243304_2@2x.jpg?1466465108000"
+            alt="profile pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
