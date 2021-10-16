a96195c0caae4dbc4aeeee8c36282a85c5a484b5
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-10-16 12:05:51 -0700

    added the posts component to the build and used dummy data for development

---
+++ insta-2-yt/components/Posts.js
@@ -0,0 +1,19 @@
+import Post from "./Post"
+
+const DUMMY_DATA = [ {
+  id: "213",
+  username: "admin",
+  userImg: '',
+  img: "",
+  caption: "ig yt project build"
+}]
+
+function Posts() {
+  return (
+    <div>
+      <Post />
+    </div>
+  )
+}
+
+export default Posts
