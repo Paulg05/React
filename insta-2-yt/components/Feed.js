3c23c9c50b814d187b4a7659f449993ef9ce6548
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-10-29 18:03:42 -0700

    finished suggestions component and fixed styling bug

--- insta-2-yt/components/Feed.js
+++ insta-2-yt/components/Feed.js
@@ -1,12 +1,15 @@
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl 
-    xl:grid-cols-3 xl:max-w-6xl mx-auto">
-      <section className="col-span-2" >
+    <main
+      className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl 
+    xl:grid-cols-3 xl:max-w-6xl mx-auto"
+    >
+      <section className="col-span-2">
         {/* Stories */}
         <Stories />
         {/* Posts */}
@@ -14,9 +17,11 @@ function Feed() {
       </section>
 
       {/* Section */}
-      <section>
-        <MiniProfile />
-        {/* Suggestions */}
+      <section className="hidden xl:inline-grid md:col-span-1">
+        <div className="fixed" >
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
     </main>
   );
