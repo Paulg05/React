def7e6d70f2743ceb60c6a082be7fd5ff64b8f6f
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-12-02 14:34:59 -0800

    created a layer component to add to the _app component to render out our header component

--- cms_blog/pages/_app.js
+++ cms_blog/pages/_app.js
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+import React, { useEffect, useState } from "react";
+import "tailwindcss/tailwind.css";
+import { Layout } from "../components";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
