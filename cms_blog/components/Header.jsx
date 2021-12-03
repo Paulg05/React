1abc3cde023d151605f75dca51c4d7095ba5fc32
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-12-03 11:30:05 -0800

    added category topics  to category components  and adjusted the logic to the header component

--- cms_blog/components/Header.jsx
+++ cms_blog/components/Header.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 
+import { getCategories } from "../services";
+
 const Header = () => {
-  const categories = [
-    { name: "React", slug: "react" },
-    { name: "Web Development", slug: "web-dev" },
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
 
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -17,15 +20,15 @@ const Header = () => {
             </span>
           </Link>
         </div>
-          <div className="hidden md:float-left md:contents">
-            {categories.map((category) => (
-              <Link key={category.slug} href={`/category/${category.slug}`}>
-                <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                  {category.name}
-                </span>
-              </Link>
-            ))}
-          </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
